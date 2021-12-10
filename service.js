c3c47c9b8b9b0686920b69a1245d3973c58a8745
Eduardo Ferreira <eduardo@ocdigital.com.br>
2021-12-10 17:28:32 -0300

    inicio

---
+++ service.js
@@ -0,0 +1,16 @@
+﻿var Service = require('node-windows').Service;
+
+// Create a new service object
+var svc = new Service({
+  name:'retornowhatsapp',
+  description: 'Integração entre os servidores',
+  script: 'C:\\MGA\\retornowhatsapp'
+});
+
+// Listen for the "install" event, which indicates the
+// process is available as a service.
+svc.on('install',function(){
+  svc.start();
+});
+
+svc.install();
